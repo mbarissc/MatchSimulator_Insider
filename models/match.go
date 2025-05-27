4d183be9a1d00421808cb9155ffc4867db079eef
Mehmet Barış Saç <141871933+mbarissc@users.noreply.github.com>
2025-05-27 14:03:01 +0300

    .

--- models/match.go
+++ models/match.go
@@ -5,7 +5,7 @@ type Match struct {
 	Week       int  `json:"week"`
 	HomeTeamID int  `json:"home_team_id"`
 	AwayTeamID int  `json:"away_team_id"`
-	HomeGoals  *int `json:"home_goals,omitempty"` // Maç oynanmadıysa null olabilir
-	AwayGoals  *int `json:"away_goals,omitempty"` // Maç oynanmadıysa null olabilir
+	HomeGoals  *int `json:"home_goals,omitempty"` 
+	AwayGoals  *int `json:"away_goals,omitempty"` 
 	IsPlayed   bool `json:"is_played"`
 }
